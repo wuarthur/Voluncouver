cb616abce72411ee7c713ec70356c99114fa481d
hantaom <hantaom@gmail.com>
2016-07-04 13:30:48 -0700

    initial commit

---
+++ app/src/main/java/com/example/hanta/voluncouver/MainPage.java
@@ -0,0 +1,14 @@
+package com.example.hanta.voluncouver;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class MainPage extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+
+}
